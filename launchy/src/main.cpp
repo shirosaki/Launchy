c6c37063614aafc967c76ed82e59c0ae94a1f893
Hiroshi Shirosaki <h.shirosaki@gmail.com>
2019-01-15 13:29:54 +0900

    Delete objects which don't have a QObject parent
    https://forum.qt.io/topic/31863/i-need-to-delete-objects-in-mainwindow/5

--- launchy/src/main.cpp
+++ launchy/src/main.cpp
@@ -286,32 +286,7 @@ LaunchyWidget::LaunchyWidget(CommandFlags command) :
 
 LaunchyWidget::~LaunchyWidget()
 {
-    delete updateTimer;
-    delete dropTimer;
-    delete alternatives;
-    delete fader;
-    delete optionsButton;
-    delete closeButton;
-    delete output;
-    delete input;
-    delete outputIcon;
-    delete workingAnimation;
-    delete listDelegate;
-    delete alternativesPath;
     delete gBuilder;
-    delete actShow;
-    delete actRebuild;
-    delete actReloadSkin;
-    delete actOptions;
-    if (frameGraphic)
-    {
-	    delete frameGraphic;
-    }
-    if (trayIcon)
-    {
-	    delete trayIcon;
-
-    }
     if (condensedTempIcon)
     {
 	    delete condensedTempIcon;
